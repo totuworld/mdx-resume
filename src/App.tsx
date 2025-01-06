39e1e68a054b35028da3dddf159400405a31ff65
totuworld <totuworld@woowahan.com>
2025-01-06 10:46:11 +0900

    :lipstick: 소소한 수정

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import Resume from "./resume.mdx";
-
-export default function App() {
-  return (
-    <Resume />
-  );
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
+import Resume from "./resume.mdx";
+import Newbie from "./newbie.mdx";
+import Superrookie from "./superrookie.mdx";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Resume />,
+  },
+  {
+    path: '/newbie',
+    element: <Newbie />,
+  },
+  {
+    path: '/superrookie',
+    element: <Superrookie />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
